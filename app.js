8b369eb0330f24b474b68839e8e30e6ac501e536
mmrn <mmrn@jp.ibm.com>
2016-11-24 17:23:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-/*eslint-env node*/
-
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
@@ -21,7 +15,6 @@ app.use(express.static(__dirname + '/public'));
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-
 /***ここから追加***/
 // POSTパラメータ取得用 body-parser設定 (express4から必要)
 var bodyParser = require('body-parser');
@@ -30,20 +23,78 @@ app.use(bodyParser.json());
 
 // Date()で現在時刻を取得するためのユーティリティ
 var dateutil = require('date-utils');
+
+// Cloudant用アクセス・モジュール「cradle」設定
+var cradle = require('cradle');
+
+// Cloudant DB接続情報取得
+var services = JSON.parse(process.env.VCAP_SERVICES);
+var credentials = services['cloudantNoSQLDB'][0].credentials;
+var host = credentials.host;
+var port = credentials.port;
+var options = {
+ cache : true,
+ raw : false,
+ secure : true,
+ auth : {
+ username : credentials.username,
+ password : credentials.password
+ }
+};
+
+// データベース接続
+var db = new (cradle.Connection)(host, port, options).database('cldb');
  
 app.post('/', function(req, res){
  var date = new Date();
  var now = date.toFormat("YYYY/MM/DD HH24:MI:SS");
  req.body.date = now;
- 
- console.log('app.js req.body: ' + JSON.stringify(req.body));
- res.send(req.body);
-});
-/***ここまで追加***/
 
+ // 項目の保存
+ db.save(now, req.body);
+  res.send(req.body);
+});
+
+//「全件削除」ボタンの id=removeAll, ui_item.jsの url:'/removeAll'でcall
+app.post('/removeAll', function(req, res){
+
+ // 全件検索を、作成したview名 items_view にて実行
+ db.view('items/items_view', function (err, rows) {
+ if (!err) {
+ rows.forEach(function (id, row) {
+ db.remove(id);
+ console.log("removed key is: %s", id);
+ });
+ } else { console.log("app.js db.remove error: " + err); }
+
+ });
+
+ res.send({});
+});
+
+
+//「全件表示」ボタンの id=getAll, ui_item.jsの url:'/getAll'でcall
+app.post('/getAll', function(req, res){
+ returnTable(res);
+});
+
+var returnTable = function(res) {
+ // 全件検索を、作成したview名 items_view にて実行
+ db.view('items/items_view', function (err, rows) {
+ if (!err) {
+ rows.forEach(function (id, row) {
+ console.log("key: %s, row: %s", id, JSON.stringify(row));
+ });
+ } else { console.log("app.js returnTable error: " + err); }
+
+ res.send(rows);
+ });
+}
+/***ここまで追加***/
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+
+ // print a message when the server starts listening
+ console.log("server starting on " + appEnv.url);
 });
